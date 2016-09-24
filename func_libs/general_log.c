fd296dc0bb7e9194153a68acf5c4c6ee14516156
Felix <xkfz007@users.noreply.github.com>
2016-09-24 12:36:36 +0800

    use array instead of switch/case

--- func_libs/general_log.c
+++ func_libs/general_log.c
@@ -1,37 +1,33 @@
 #include "general_log.h"
+#include <errno.h>
+#include <stdarg.h>
 static void general_log(const char* caller, int level, const char* fmt, ...)
 {
-    const int bufferSize = 4096;
+#define  bufferSize 4096
     char buffer[bufferSize];
     int p = 0;
-    const char* log_level;
-    switch (level)
-    {
-    case GENL_LOG_ERROR:
-        log_level = "error";
-        break;
-    case GENL_LOG_WARNING:
-        log_level = "warning";
-        break;
-    case GENL_LOG_INFO:
-        log_level = "info";
-        break;
-    case GENL_LOG_DEBUG:
-        log_level = "debug";
-        break;
-    case GENL_LOG_FULL:
-        log_level = "full";
-        break;
-    default:
-        log_level = "unknown";
-        break;
-    }
-
-    if (caller)
-        p += sprintf(buffer, "%-4s [%s]: ", caller, log_level);
     va_list arg;
+	const char* log_level[]={
+		"Unknown",
+		"Error",
+		"Warning",
+		"Info",
+		"Debug",
+		"Full",
+	};
+    if (caller)
+        p += sprintf(buffer, "[%-4s]", caller);
+	if(level>GENL_LOG_FULL)
+		level=GENL_LOG_NONE;
+	if(level==GENL_LOG_ERROR)
+		p += sprintf(buffer, "[%s]:[Errno=%d Err=%s]: ", log_level[level],errno,strerror(errno));
+	else
+		p += sprintf(buffer, "[%s]: ", log_level[level]);
+
     va_start(arg, fmt);
     vsnprintf(buffer + p, bufferSize - p, fmt, arg);
     va_end(arg);
     fputs(buffer, stderr);
+
+#undef bufferSize
 }
